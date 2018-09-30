b06856dddc659ac7af750b804a1c7086e847e7ff
gaurav9969351313 <gaurav.cdac16@gmail.com>
2018-09-30 18:31:44 +0530

    message

---
+++ gitPush.bat
@@ -0,0 +1,9 @@
+dir 
+git config --global user.name "gaurav9969351313"
+git config --global user.email "gaurav.cdac16@gmail.com"
+
+git add .
+git commit -m "message"
+git push origin master
+
+pause
